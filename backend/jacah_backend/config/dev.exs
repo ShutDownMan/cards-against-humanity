34e12aaf713c40dd3d654bd97c7c40df0aa7f9cf
ShutDownMan <Jedson_Gabriel@hotmail.com>
2022-12-06 21:39:54 -0300

    attempting to dockerize the elixir backend

--- backend/jacah_backend/config/dev.exs
+++ backend/jacah_backend/config/dev.exs
@@ -2,11 +2,11 @@ import Config
 
 # Configure your database
 config :jacah_backend, JacahBackend.Repo,
-  username: System.get_env("POSTGRES_USER") || "jacah_user",
-  password: System.get_env("POSTGRES_PASSWORD") || "123456sete",
-  hostname: System.get_env("POSTGRES_ADDRESS") || "localhost",
-  database: System.get_env("POSTGRES_DB") || "jacah_db",
-  port: System.get_env("POSTGRES_PORT") || "5443",
+  username: System.get_env("POSTGRES_USER", "jacah_user"),
+  password: System.get_env("POSTGRES_PASSWORD", "123456sete"),
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  database: System.get_env("POSTGRES_DB", "jacah_db"),
+  port:  System.get_env("POSTGRES_PORT", "5443"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -21,6 +21,12 @@ config :jacah_backend, JacahBackendWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   http: [ip: {127, 0, 0, 1}, port: 4000],
+  https: [
+    port: 4001,
+    cipher_suite: :strong,
+    certfile: "priv/cert/selfsigned.pem",
+    keyfile: "priv/cert/selfsigned_key.pem"
+  ],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
